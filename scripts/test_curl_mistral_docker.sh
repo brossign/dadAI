140bdb639fae33eaea43d295b090e6a1f8c7696f
Benoit Rossignol <benoit.rossignol@gmail.com>
2025-04-23 19:57:52 +0200

    Nettoyage LocalAI / Docker - préparation fine-tuning RunPod

--- scripts/test_curl_mistral_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl http://localhost:8080/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -d '{
-    "model": "mistral",
-    "messages": [{"role": "user", "content": "Je suis un nouveau papa. Comment soutenir ma femme ?"}]
-}'
